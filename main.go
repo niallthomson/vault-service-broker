39f993ee55e989f8bb5d22ef77d7ce671fc3bcb1
Becca Petrin <beccapetrin@posteo.net>
2019-01-08 16:29:10 -0800

    parse from credhub if exists

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -15,59 +17,8 @@ import (
 	"github.com/cloudfoundry-community/go-credhub"
 	"github.com/hashicorp/go-cleanhttp"
 	"github.com/hashicorp/vault/api"
+	"github.com/kelseyhightower/envconfig"
 	"github.com/pivotal-cf/brokerapi"
-)
-
-const (
-	// If settings are stored in CredHub, the prefix for all relevant settings.
-	// For example: VAULT_SERVICE_BROKER_SECURITY_USER_NAME
-	CredhubPrefix = "VAULT_SERVICE_BROKER_"
-
-	// REQUIRED CONFIG PARAMETERS
-	SecurityUserName     = "SECURITY_USER_NAME"
-	SecurityUserPassword = "SECURITY_USER_PASSWORD"
-	VaultToken           = "VAULT_TOKEN"
-
-	// OPTIONAL CONFIG PARAMETERS
-	CredhubURL = "CREDHUB_URL"
-
-	Port        = "PORT"
-	DefaultPort = ":8000"
-
-	// ServiceID is the UUID of the services
-	ServiceID        = "SERVICE_ID"
-	DefaultServiceID = "0654695e-0760-a1d4-1cad-5dd87b75ed99"
-
-	// VaultAdvertiseAddr is the address of the Vault cluster that this service broker should use,
-	// and that should be used by USERS of the service broker.
-	// If it's unset, it'll fall back to the VaultAddr. It's not necessary to have both,
-	// but previous code was written this way and they're both retained for backwards compatibility.
-	VaultAddr          = "VAULT_ADDR"
-	DefaultVaultAddr   = "https://127.0.0.1:8200"
-	VaultAdvertiseAddr = "VAULT_ADVERTISE_ADDR"
-
-	// ServiceName is the name of the service in the marketplace
-	ServiceName        = "SERVICE_NAME"
-	DefaultServiceName = "hashicorp-vault"
-
-	// ServiceDescription is the service description in the marketplace
-	ServiceDescription        = "SERVICE_DESCRIPTION"
-	DefaultServiceDescription = "HashiCorp Vault Service Broker"
-
-	// PlanName is the name of our plan, only one is supported
-	PlanName        = "PLAN_NAME"
-	DefaultPlanName = "shared"
-
-	// PlanDescription is the plan's description
-	PlanDescription        = "PLAN_DESCRIPTION"
-	DefaultPlanDescription = "Secure access to Vault's storage and transit backends"
-
-	// These are optional; if not provided, none will be added
-	ServiceTags = "SERVICE_TAGS"
-
-	// Denotes whether the service broker should automatically renew the service broker's token
-	VaultRenew        = "VAULT_RENEW"
-	DefaultVaultRenew = "true"
 )
 
 func main() {
@@ -75,31 +26,21 @@ func main() {
 	// be prefixed in the log output by CF
 	logger := log.New(os.Stdout, "", 0)
 
-	h := newSettingHandler(logger, os.Getenv(CredhubURL))
-
-	// Parse required settings
-	username := h.GetOrDefault(SecurityUserName, "")
-	if username == "" {
-		logger.Fatalf("[ERR] missing %s", SecurityUserName)
-	}
-	password := h.GetOrDefault(SecurityUserPassword, "")
-	if password == "" {
-		logger.Fatalf("[ERR] missing %s", SecurityUserPassword)
-	}
-	if vaultToken := h.GetOrDefault(VaultToken, ""); vaultToken == "" {
-		logger.Fatalf("[ERR] missing %s", VaultToken)
+	// Parse the config from the environment
+	config := &Configuration{}
+	if err := envconfig.Process("", config); err != nil {
+		log.Fatalf("failed to parse environment variables: %s", err)
 	}
 
-	// Parse optional settings
-	vaultAddr := normalizeAddr(h.GetOrDefault(VaultAddr, DefaultVaultAddr))
-	vaultAdvertiseAddr := h.GetOrDefault(VaultAdvertiseAddr, vaultAddr)
-	port := h.GetOrDefault(Port, DefaultPort)
-	if port[0] != ':' {
-		port = ":" + port
+	if config.CredhubURL != "" {
+		// Override env vars with anything set in credhub
+		if err := credhubProcess("VAULT_SERVICE_BROKER_", config); err != nil {
+			log.Fatalf("failed to parse credhub settings: %s", err)
+		}
 	}
-	renew, err := strconv.ParseBool(h.GetOrDefault(VaultRenew, DefaultVaultRenew))
-	if err != nil {
-		logger.Fatalf("[ERR] failed to parse %s: %s", VaultRenew, err)
+
+	if err := config.Validate(); err != nil {
+		logger.Fatalf("[ERR] config validation failed: %s", err)
 	}
 
 	// Setup the vault vaultClient
@@ -113,16 +54,16 @@ func main() {
 		log:    logger,
 		client: client,
 
-		serviceID:          h.GetOrDefault(ServiceID, DefaultServiceID),
-		serviceName:        h.GetOrDefault(ServiceName, DefaultServiceName),
-		serviceDescription: h.GetOrDefault(ServiceDescription, DefaultServiceDescription),
-		serviceTags:        strings.Split(os.Getenv(ServiceTags), ","),
+		serviceID:          config.ServiceID,
+		serviceName:        config.ServiceName,
+		serviceDescription: config.ServiceDescription,
+		serviceTags:        config.ServiceTags,
 
-		planName:        h.GetOrDefault(PlanName, DefaultPlanName),
-		planDescription: h.GetOrDefault(PlanDescription, DefaultPlanDescription),
+		planName:        config.PlanName,
+		planDescription: config.PlanDescription,
 
-		vaultAdvertiseAddr: vaultAdvertiseAddr,
-		vaultRenewToken:    renew,
+		vaultAdvertiseAddr: config.VaultAdvertiseAddr,
+		vaultRenewToken:    config.VaultRenew,
 	}
 	if err := broker.Start(); err != nil {
 		logger.Fatalf("[ERR] failed to start broker: %s", err)
@@ -130,8 +71,8 @@ func main() {
 
 	// Parse the broker credentials
 	creds := brokerapi.BrokerCredentials{
-		Username: username,
-		Password: password,
+		Username: config.SecurityUserName,
+		Password: config.SecurityPassword,
 	}
 
 	// Setup the HTTP handler
@@ -140,8 +81,8 @@ func main() {
 	// Listen to incoming connection
 	serverCh := make(chan struct{}, 1)
 	go func() {
-		logger.Printf("[INFO] starting server on %s", port)
-		if err := http.ListenAndServe(port, handler); err != nil {
+		logger.Printf("[INFO] starting server on %s", config.Port)
+		if err := http.ListenAndServe(config.Port, handler); err != nil {
 			logger.Fatalf("[ERR] server exited with: %s", err)
 		}
 		close(serverCh)
@@ -206,51 +147,91 @@ func normalizeAddr(s string) string {
 	return u.String()
 }
 
-func newSettingHandler(logger *log.Logger, credhubURL string) *settingHandler {
-	// TODO client authorization? Should I assume I don't need to worry about this if they've set up certs correctly?
-	if credhubURL == "" {
-		return &settingHandler{logger, nil}
-	}
-	return &settingHandler{logger, credhub.New(credhubURL, cleanhttp.DefaultClient())}
+type Configuration struct {
+	// Required
+	SecurityUserName string `envconfig:"security_user_name"`
+	SecurityPassword string `envconfig:"security_user_password"`
+	VaultToken       string `envconfig:"vault_token"`
+
+	// Optional
+	CredhubURL         string     `envconfig:"credhub_url"`
+	Port               string     `envconfig:"port" default:":8000"`
+	ServiceID          string     `envconfig:"service_id" default:"0654695e-0760-a1d4-1cad-5dd87b75ed99"`
+	VaultAddr          string     `envconfig:"vault_addr" default:"https://127.0.0.1:8200"`
+	VaultAdvertiseAddr string     `envconfig:"vault_advertise_addr"`
+	ServiceName        string     `envconfig:"service_name" default:"hashicorp-vault"`
+	ServiceDescription string     `envconfig:"service_description" default:"HashiCorp Vault Service Broker"`
+	PlanName           string     `envconfig:"plan_name" default:"shared"`
+	PlanDescription    string     `envconfig:"plan_description" default:"Secure access to Vault's storage and transit backends"`
+	ServiceTags        []string   `envconfig:"service_tags"`
+	VaultRenew         bool       `envconfig:"vault_renew" default:"true"`
 }
 
-type settingHandler struct {
-	logger        *log.Logger
-	credhubClient *credhub.Client
-}
-
-func (h *settingHandler) GetOrDefault(settingName, settingDefault string) string {
-	if h.credhubClient == nil {
-		return h.checkEnv(settingName, settingDefault)
+func (c *Configuration) Validate() error {
+	// Ensure required parameters were provided
+	if c.SecurityUserName == "" {
+		return errors.New("missing SECURITY_USER_NAME")
+	}
+	if c.SecurityPassword == "" {
+		return errors.New("missing SECURITY_USER_PASSWORD")
+	}
+	if c.VaultToken == "" {
+		return errors.New("missing VAULT_TOKEN")
 	}
 
-	latest, err := h.credhubClient.GetLatestByName(CredhubPrefix + settingName)
-	if err != nil {
-		// "Name Not Found" is returned when the CredHub API returns a 404.
-		// It's not ideal that we need to match a string
-		// but there's nothing better to check against.
-		// In case the error string changes slightly with future SDK versions,
-		// we match flexibly.
-		if strings.Contains(strings.ToLower(err.Error()), "not found") {
-			// This variable isn't stored in CredHub, see if it's an env variable.
-			return h.checkEnv(settingName, settingDefault)
+	// If these values aren't perfect, we can fix them
+	if c.Port[0] != ':' {
+		c.Port = ":" + c.Port
+	}
+	if c.VaultAdvertiseAddr == "" {
+		c.VaultAdvertiseAddr = c.VaultAddr
+	}
+	c.VaultAddr = normalizeAddr(c.VaultAddr)
+	c.VaultAdvertiseAddr = normalizeAddr(c.VaultAdvertiseAddr)
+	return nil
+}
+
+func credhubProcess(prefix string, config *Configuration) error {
+
+	client := credhub.New(config.CredhubURL, cleanhttp.DefaultClient())
+
+	// Pull the "envconfig" field name from each field and look for it in Credhub
+	configTypeInfo := reflect.TypeOf(*config)
+	settableConfig := reflect.ValueOf(config).Elem()
+	for i := 0; i < configTypeInfo.NumField(); i++ {
+		fieldTypeInfo := configTypeInfo.Field(i)
+
+		credhubName := prefix + strings.ToUpper(fieldTypeInfo.Tag.Get("envconfig"))
+		latest, err := client.GetLatestByName(credhubName)
+		if err != nil && !strings.Contains(strings.ToLower(err.Error()), "not found") {
+			return err
 		}
-		// Otherwise, this is a real error.
-		h.logger.Fatalf("error pulling settings from credhub: %s", err)
-	}
-	settingValue, ok := latest.Value.(string)
-	if !ok {
-		h.logger.Fatalf("we only support credhub values as string, but received %s as a %s", settingName, reflect.TypeOf(latest.Value))
-	}
-	if settingValue != "" {
-		return settingValue
-	}
-	return h.checkEnv(settingName, settingDefault)
-}
+		if latest == nil {
+			continue
+		}
+		settingValue, ok := latest.Value.(string)
+		if !ok {
+			return fmt.Errorf("we only support credhub values as bash-like string values, but received %s as a %s", credhubName, reflect.TypeOf(latest.Value))
+		}
+		if settingValue == "" {
+			continue
+		}
 
-func (h *settingHandler) checkEnv(settingName, settingDefault string) string {
-	if settingValue := os.Getenv(settingName); settingValue != "" {
-		return settingValue
+		settableField := settableConfig.Field(i)
+		switch fieldTypeInfo.Type.Kind() {
+		case reflect.Bool:
+			asBool, err := strconv.ParseBool(settingValue)
+			if err != nil {
+				return fmt.Errorf("error parsing bool %s: %s", credhubName, err)
+			}
+			settableField.SetBool(asBool)
+		case reflect.String:
+			settableField.SetString(settingValue)
+		case reflect.Slice:
+			settableField.Set(reflect.ValueOf(strings.Split(settingValue, ",")))
+		default:
+			return fmt.Errorf("unsupported type of %s for %s", fieldTypeInfo.Type.Kind(), credhubName)
+		}
 	}
-	return settingDefault
+	return nil
 }
